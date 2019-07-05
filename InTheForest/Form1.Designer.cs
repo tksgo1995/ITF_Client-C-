77afab3f38b6fff74a79eadaf544ae6c779dc02e
tksgo1995 <36852046+tksgo1995@users.noreply.github.com>
2019-07-05 18:10:38 +0900

    C# 에이전트 v1.0

---
+++ InTheForest/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace InTheForest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView_Window = new System.Windows.Forms.ListView();
+            this.treeView_Window = new System.Windows.Forms.TreeView();
+            this.button_Back = new System.Windows.Forms.Button();
+            this.button_Front = new System.Windows.Forms.Button();
+            this.label_Path = new System.Windows.Forms.Label();
+            this.process_FileStart = new System.Diagnostics.Process();
+            this.SuspendLayout();
+            // 
+            // listView_Window
+            // 
+            this.listView_Window.Location = new System.Drawing.Point(210, 65);
+            this.listView_Window.Name = "listView_Window";
+            this.listView_Window.Size = new System.Drawing.Size(578, 373);
+            this.listView_Window.TabIndex = 0;
+            this.listView_Window.UseCompatibleStateImageBehavior = false;
+            this.listView_Window.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView_Window_DragDrop);
+            this.listView_Window.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView_Window_DragEnter);
+            this.listView_Window.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView_Window_KeyDown);
+            this.listView_Window.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView_Window_MouseDoubleClick);
+            // 
+            // treeView_Window
+            // 
+            this.treeView_Window.Location = new System.Drawing.Point(12, 65);
+            this.treeView_Window.Name = "treeView_Window";
+            this.treeView_Window.Size = new System.Drawing.Size(192, 373);
+            this.treeView_Window.TabIndex = 1;
+            this.treeView_Window.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_Window_BeforeExpand);
+            this.treeView_Window.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_Window_AfterSelect);
+            this.treeView_Window.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_Window_NodeMouseClick);
+            // 
+            // button_Back
+            // 
+            this.button_Back.Location = new System.Drawing.Point(12, 36);
+            this.button_Back.Name = "button_Back";
+            this.button_Back.Size = new System.Drawing.Size(75, 23);
+            this.button_Back.TabIndex = 2;
+            this.button_Back.Text = "button1";
+            this.button_Back.UseVisualStyleBackColor = true;
+            this.button_Back.Click += new System.EventHandler(this.button_Back_Click);
+            // 
+            // button_Front
+            // 
+            this.button_Front.Location = new System.Drawing.Point(93, 36);
+            this.button_Front.Name = "button_Front";
+            this.button_Front.Size = new System.Drawing.Size(75, 23);
+            this.button_Front.TabIndex = 3;
+            this.button_Front.Text = "button2";
+            this.button_Front.UseVisualStyleBackColor = true;
+            this.button_Front.Click += new System.EventHandler(this.button_Front_Click);
+            // 
+            // label_Path
+            // 
+            this.label_Path.AutoSize = true;
+            this.label_Path.Location = new System.Drawing.Point(210, 47);
+            this.label_Path.Name = "label_Path";
+            this.label_Path.Size = new System.Drawing.Size(0, 12);
+            this.label_Path.TabIndex = 4;
+            // 
+            // process_FileStart
+            // 
+            this.process_FileStart.StartInfo.Domain = "";
+            this.process_FileStart.StartInfo.LoadUserProfile = false;
+            this.process_FileStart.StartInfo.Password = null;
+            this.process_FileStart.StartInfo.StandardErrorEncoding = null;
+            this.process_FileStart.StartInfo.StandardOutputEncoding = null;
+            this.process_FileStart.StartInfo.UserName = "";
+            this.process_FileStart.SynchronizingObject = this;
+            this.process_FileStart.Exited += new System.EventHandler(this.process_FileStart_Exited);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_Path);
+            this.Controls.Add(this.button_Front);
+            this.Controls.Add(this.button_Back);
+            this.Controls.Add(this.treeView_Window);
+            this.Controls.Add(this.listView_Window);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_Window;
+        private System.Windows.Forms.TreeView treeView_Window;
+        private System.Windows.Forms.Button button_Back;
+        private System.Windows.Forms.Button button_Front;
+        private System.Windows.Forms.Label label_Path;
+        private System.Diagnostics.Process process_FileStart;
+    }
+}
+
---
+++ InTheForest/Form1.cs
@@ -0,0 +1,328 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InTheForest
+{
+    public partial class Form1 : Form
+    {
+        AES k;
+        private static EventWaitHandle waitforsinglesignal;
+        private int mask;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] drives = Directory.GetLogicalDrives();// 모든 논리적으로 구분되어 있는 드라이브들을 읽어들임
+
+            foreach (string drive in drives)
+            {
+                DriveInfo di = new DriveInfo(drive);
+
+                if (di.IsReady)// 드라이브가 준비되었는지 여부 판단. 이 조건으로 CD R/W 드라이브 등을 제외시킴.
+                {
+                    TreeNode node = treeView_Window.Nodes.Add(drive);
+                    node.Nodes.Add("\\");    // 파일 경로 사이사이에 "\"를 추가함.
+                }
+            }
+            k = new AES(); // AES 파일 암복호화에 사용할 키를 초기화
+            waitforsinglesignal = new EventWaitHandle(false, EventResetMode.AutoReset); // waitforsingleobject()
+        }
+
+        private void SettingListView(string sFullPath) // 리스트뷰에 현위치에있는 리스트출력함수
+        {
+            try
+            {
+                // 기존의 파일 목록 제거
+                listView_Window.Items.Clear();
+                DirectoryInfo dir = new DirectoryInfo(sFullPath);
+
+                int DirectCount = 0;
+                // 하부 디렉토리 보여주기
+                foreach (DirectoryInfo dirItem in dir.GetDirectories())
+                {
+                    // 하부 디렉토리가 존재할 경우 ListView에 추가
+                    // ListViewItem 객체를 생성
+                    ListViewItem lsvitem = new ListViewItem();
+
+                    // 생성된 ListViewItem 객체에 똑같은 이미지를 할당
+                    lsvitem.ImageIndex = 2;
+                    lsvitem.Text = dirItem.Name;
+
+                    // 아이템을 ListView(listView1)에 추가
+                    listView_Window.Items.Add(lsvitem);
+
+                    listView_Window.Items[DirectCount].SubItems.Add(dirItem.CreationTime.ToString());
+                    listView_Window.Items[DirectCount].SubItems.Add("폴더");
+                    listView_Window.Items[DirectCount].SubItems.Add(dirItem.GetFiles().Length.ToString() +
+                        "files");
+                    DirectCount++;
+                }
+
+                // 디렉토리에 존재하는 파일목록 보여주기
+                FileInfo[] files = dir.GetFiles();
+                int Count = 0;
+                foreach (FileInfo fileinfo in files)
+                {
+                    ListViewItem lsvitem = new ListViewItem();
+                    lsvitem.ImageIndex = 4;
+                    lsvitem.Text = fileinfo.Name;
+                    listView1.Items.Add(lsvitem);
+
+                    if (fileinfo.LastWriteTime != null)
+                    {
+                        listView1.Items[Count].SubItems.Add(fileinfo.LastWriteTime.ToString());
+                    }
+                    else
+                    {
+                        listView1.Items[Count].SubItems.Add(fileinfo.CreationTime.ToString());
+                    }
+                    listView1.Items[Count].SubItems.Add(fileinfo.Attributes.ToString());
+                    listView1.Items[Count].SubItems.Add(fileinfo.Length.ToString());
+                    Count++;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("에러 발생 : " + ex.Message);
+            }
+        }
+
+        public bool GET_BIT(int x, int y) // 서버에서 시스템 정책 마스크값 가져오면 한비트씩 빼내기
+        {
+            if ((((x) >> (y)) & 0x01) == 1) return true;
+            return false;
+        }
+
+        public void UpdateRegistry(string root, int value, string name) // 정책에 맞게 레지스트리 업데이트
+        {
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey(root, true);
+            if (reg == null)
+            {
+                // 해당이름으로 서브키 생성
+                reg = Registry.CurrentUser.CreateSubKey(root);
+            }
+            reg.SetValue(name, value);
+            reg.Close();
+        }
+
+        public void UpdatePolicy() // 마스크값에 따라 시스템 정책변경
+        {
+            int value;
+            // 1. TaskMgr enable(0), disable(11)
+            if (GET_BIT(mask, 0)) value = 1;
+            else value = 0;
+            UpdateRegistry("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", value, "DisableTaskMgr");
+
+            // 2. regedit enable(0), disable(1)
+            if (GET_BIT(mask, 1)) value = 1;
+            else value = 0;
+            UpdateRegistry("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", value, "DisableRegistryTools");
+
+            // 3. cmd enable(0), disable(2)
+            if (GET_BIT(mask, 2)) value = 2;
+            else value = 0;
+            UpdateRegistry("Software\\Policies\\Microsoft\\Windows\\System", value, "DisableCMD");
+
+            // 4. snipping tools disable(1), enable(0)
+            if (GET_BIT(mask, 3)) value = 1;
+            else value = 0;
+            UpdateRegistry("SOFTWARE\\Policies\\Microsoft\\TabletPC", value, "DisableSnippingTool");
+
+            // 5. usb쓰기 disable(1), enable(0)
+            if (GET_BIT(mask, 4)) value = 1;
+            else value = 0;
+            UpdateRegistry("SYSTEM\\CurrentControlSet\\Control\\StorageDevicepolicies", value, "WriteProtect");
+
+            // 6. usb차단 disable(4) enable(3)
+            if (GET_BIT(mask, 5)) value = 4;
+            else value = 3;
+            UpdateRegistry("SYSTEM\\CurrentControlSet\\Services\\USBSTOR", value, "Start");
+
+            // 7. 디스크차단(C드라이브) disable(4) enable(0)
+            if (GET_BIT(mask, 6)) value = 4;
+            else value = 0;
+            UpdateRegistry("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", value, "NoViewOnDrive");
+        }
+
+        private void treeView_Window_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            SettingListView(e.Node.FullPath);
+        }
+
+        private void listView_Window_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                //back = label1.Text;
+                byte[] decbytes;
+                ListViewItem item = listView_Window.SelectedItems[0];
+                string file = label_Path.Text + "\\" + item.Text;
+
+                FileAttributes attr = File.GetAttributes(file);
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    label_Path.Text = file;
+                    SettingListView(file);
+                }
+                else
+                {
+                    if (file.Contains(".enc"))
+                    {
+                        Thread t1 = new Thread(new ThreadStart(Run));
+                        t1.Start();
+                        waitforsinglesignal.WaitOne();
+                        void Run()
+                        {
+                            decbytes = k.AESDecrypto256(File.ReadAllBytes(file), key);
+                            file = file.Replace(".enc", "");
+                            MessageBox.Show(file);
+                            File.WriteAllBytes(file, decbytes);
+                            waitforsinglesignal.Set();
+                        }
+
+                        //Thread.Sleep(1000);
+                    }
+                    string filename = Path.GetFileName(file);
+                    process_FileStart.StartInfo.FileName = filename;
+                    process_FileStart.StartInfo.WorkingDirectory = label_Path.Text;
+                    process_FileStart.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    process_FileStart.Start();
+
+                    SettingListView(label_Path.Text);
+                }
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("에러 : " + e1.Message);
+            }
+        }
+
+        private void treeView_Window_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text.Equals("\\"))
+                {
+                    e.Node.Nodes.Clear();
+
+                    string path = e.Node.FullPath;
+                    //string path = e.Node.FullPath.Substring(e.Node.FullPath.IndexOf("\\") + 1).Replace("\\\\", "\\");
+                    //실제 절대 경로를 path에 저장합니다.
+                    //MessageBox.Show(path);
+                    string[] directories = Directory.GetDirectories(path);
+                    // 현재 경로에서 존재하고 있는 폴더들을 하위 노드에 추가
+                    //Directory.GetDirectories() 함수를 이용하여 인자에 입력된 경로상의 하위 디렉토리의 이름과 경로를 반환 시킵니다.
+                    //하위 디렉토리가 여러개일 수 있으므로 string 배열로 저장 받습니다.
+                    foreach (string directory in directories)//반복을 통해 트리 탐색창 의 노드에 추가시켜 화면에 하위 디렉토리를 보여주니다.
+                    {
+                        TreeNode newNode = e.Node.Nodes.Add(directory.Substring(directory.LastIndexOf("\\") + 1));
+                        newNode.Nodes.Add("\\");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("treeView1_BeforeExpand : " + ex.Message);
+            }
+        }
+
+        private void treeView_Window_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode current = e.Node;
+            string path = current.FullPath;
+            //MessageBox.Show(path);
+
+            label1.Text = path.Replace("\\\\", "\\");
+            try
+            {
+                if (label1.Text == "내 컴퓨터" || path == "Root")
+                {
+                    //webBr.Url = new Uri("C:\\");
+                    label1.Text = "C:\\";
+                }
+                else
+                {
+                    //MessageBox.Show(path);
+                    //webBr.Url = new Uri(TaxtPath.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("예외 발생 : " + ex);
+                //throw;
+            }
+        }
+
+        private void listView_Window_DragDrop(object sender, DragEventArgs e)
+        {
+            string filename;
+            byte[] encbytes;
+            key = GetRandomPassword(32);
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files)
+            {
+                filename = Path.GetFileName(file);
+                encbytes = k.AESEncrypto256(File.ReadAllBytes(file), key);
+                File.WriteAllBytes(label1.Text + "\\" + filename + ".enc", encbytes);
+                SettingListView(label1.Text);
+            }
+        }
+
+        private void listView_Window_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listView_Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode.Equals(Keys.Delete))
+                {
+                    ListViewItem item = listView_Window.SelectedItems[0];
+                    string file = label_Path.Text + "\\" + item.Text;
+                    File.Delete(file);
+                    SettingListView(label_Path.Text);
+                }
+            }
+            catch (Exception e1)
+            {
+
+            }
+        }
+
+        private void button_Back_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Front_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void process_FileStart_Exited(object sender, EventArgs e)
+        {
+            string filename = label_Path.Text + process_FileStart.StartInfo.FileName;
+            //MessageBox.Show(filename);
+            File.Delete(filename); // 프로세스 종료시 파일삭제
+            SettingListView(label_Path.Text);
+        }
+    }
+}
