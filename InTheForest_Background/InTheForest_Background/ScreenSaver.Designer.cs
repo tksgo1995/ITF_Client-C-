96dd22813ebe1dfdc145f2a84cb0d4fb0525ac2d
tksgo1995 <36852046+tksgo1995@users.noreply.github.com>
2019-08-12 18:49:26 +0900

    백그라운드 프로그램 C++ -> C#
    백그라운드 프로그램 C++ -> C#
    
    스크린 세이버 완성

---
+++ InTheForest_Background/InTheForest_Background/ScreenSaver.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace InTheForest_Background
+{
+    partial class ScreenSaver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button_Confirm = new System.Windows.Forms.Button();
+            this.button_Shutdown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(603, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 391);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PW";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(625, 361);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(625, 388);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button_Confirm
+            // 
+            this.button_Confirm.Location = new System.Drawing.Point(625, 415);
+            this.button_Confirm.Name = "button_Confirm";
+            this.button_Confirm.Size = new System.Drawing.Size(75, 23);
+            this.button_Confirm.TabIndex = 4;
+            this.button_Confirm.Text = "로그인";
+            this.button_Confirm.UseVisualStyleBackColor = true;
+            this.button_Confirm.Click += new System.EventHandler(this.Button_Confirm_Click);
+            // 
+            // button_Shutdown
+            // 
+            this.button_Shutdown.Location = new System.Drawing.Point(706, 415);
+            this.button_Shutdown.Name = "button_Shutdown";
+            this.button_Shutdown.Size = new System.Drawing.Size(82, 23);
+            this.button_Shutdown.TabIndex = 5;
+            this.button_Shutdown.Text = "시스템 종료";
+            this.button_Shutdown.UseVisualStyleBackColor = true;
+            this.button_Shutdown.Click += new System.EventHandler(this.Button_Shutdown_Click);
+            // 
+            // ScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.button_Shutdown);
+            this.Controls.Add(this.button_Confirm);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ScreenSaver";
+            this.Text = "ScreenSaver";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ScreenSaver_FormClosing);
+            this.Load += new System.EventHandler(this.ScreenSaver_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button_Confirm;
+        private System.Windows.Forms.Button button_Shutdown;
+    }
+}
---
+++ InTheForest_Background/InTheForest_Background/ScreenSaver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InTheForest_Background
+{
+    public partial class ScreenSaver : Form
+    {
+        private int nScreenSaverFlag;
+        public int NSFlag
+        {
+            get { return nScreenSaverFlag; }
+            set { nScreenSaverFlag = value; }
+        }
+        private int nScreenSaver;
+        public int NSSaver
+        {
+            get { return nScreenSaver; }
+            set { nScreenSaver = value; }
+        }
+        public ScreenSaver()
+        {
+            InitializeComponent();
+            nScreenSaver = 0;
+        }
+
+        private void ScreenSaver_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.StartPosition = FormStartPosition.Manual;
+            this.TopMost = true;
+
+            nScreenSaverFlag = 1;
+            nScreenSaver = 0;
+        }
+
+        
+        private void Button_Confirm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Shutdown_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+
+        private void ScreenSaver_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            nScreenSaverFlag = 0;
+        }
+    }
+}
